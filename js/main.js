c7f81773ed4fd7cb3247c360d5b5daad16c48eb1
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-05-21 14:45:56 +0200

    Milestone 1

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
 'use strict'
 console.log('sono collegato')
+//Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.(vue)
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'ciao mondo!'
+      }
+    }
+  }).mount('#app')
+
+
